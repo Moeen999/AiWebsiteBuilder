0291de7f9afd850508027f144b36a350cca7b5a3
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-19 16:26:40 +0500

    Initial BE setup with neon db and prisma ORM also added the other necessary packages for BE

---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+const corsOptions = {
+    origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+
+const port = 3000;
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
