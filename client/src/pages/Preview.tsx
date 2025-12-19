208a9069d62f0e7586626e92500375b69f3997d0
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-19 13:03:24 +0500

    Refactored Preview and View components for improved code structure and loading state management

--- client/src/pages/Preview.tsx
+++ client/src/pages/Preview.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyProjects } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
+import ProjectReview from "../components/ProjectReview";
+import type { Project } from "../types";
 
 const Preview = () => {
-  return (
-    <div>
-      <h1>Preview Page</h1>
-    </div>
-  )
-}
+  const { projectId, versionId } = useParams();
 
-export default Preview
+  const [code, setCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchCode = () => {
+    setTimeout(() => {
+      const code = dummyProjects.find(
+        (project) => project.id === projectId
+      )?.current_code;
+      if (code) {
+        setCode(code);
+        setIsLoading(false);
+      }
+    }, 2000);
+  };
+
+  useEffect(() => {
+    fetchCode();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader2Icon className="size-7 animate-spin text-indigo-200" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-screen">
+      {code && (
+        <ProjectReview
+          isGenerating={false}
+          project={{ current_code: code } as Project}
+          showEditorPanel={false}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Preview;
